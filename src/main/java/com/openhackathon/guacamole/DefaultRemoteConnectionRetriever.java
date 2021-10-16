d1318e3bfc5caac9254369d1f455bc982cd21b5b
Junbo Wang <juniwang@microsoft.com>
2021-10-16 20:22:50 +0800

    upgrade dependencies

---
+++ src/main/java/com/openhackathon/guacamole/DefaultRemoteConnectionRetriever.java
@@ -0,0 +1,13 @@
+package com.openhackathon.guacamole;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class DefaultRemoteConnectionRetriever implements RemoteConnectionRetriever
+{
+    public JSONObject GetRemoteConnections(String token)
+    {
+        return null;
+    }
+}
